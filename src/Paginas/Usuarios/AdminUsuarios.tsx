9afcf6d3226dae2439c073a6a36f8d1c215020e9
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-11-11 12:16:35 -0300

    ...

--- src/Paginas/Usuarios/AdminUsuarios.tsx
+++ src/Paginas/Usuarios/AdminUsuarios.tsx
@@ -1,27 +1,30 @@
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-import { useEffect, useState } from "react"
-import IUsuario from "../../Interface/IUsuario"
-import http from "../../http"
+import { useEffect, useState } from "react";
+import Mensagem from '../../componentes/Mensagens';
+import IUsuario from "../../Interface/IUsuario";
+import http from "../../http";
 
 const AdminUsuarios = () => {
 
   const [usuarios, setUsuarios] = useState<IUsuario[]>([])
-/*   const [apagar, setApagar] = useState(false)
- */
+  const [apagar, setApagar] = useState(false)
+  const [mensagem, setMensagem] = useState("")
+
   useEffect(() => {
     http.get<IUsuario[]>('/usuarios')
-        .then(res => setUsuarios(res.data))
-  }, [] )
+      .then(res => setUsuarios(res.data))
+  }, [])
 
-  const excluirUsuario = (usuarioExcluido:IUsuario) => {
+  const excluirUsuario = (usuarioExcluido: IUsuario) => {
     http.delete(`usuarios/${usuarioExcluido._id}/`)
-        .then(()=> {
-          const listaUsuarios = usuarios.filter(usuario => usuario._id !== usuarioExcluido._id)
-          setUsuarios([...listaUsuarios])
-/*           setApagar(true)
- */        })
-/*           setApagar(false)
- */  }
+      .then(() => {
+        const listaUsuarios = usuarios.filter(usuario => usuario._id !== usuarioExcluido._id)
+        setUsuarios([...listaUsuarios])
+        setMensagem("Usuário excluído com sucesso")
+        setApagar(true)
+      })
+    setApagar(false)
+  }
 
   return (
     <>
@@ -49,23 +52,26 @@ const AdminUsuarios = () => {
                 <TableCell> {usuario.nome} </TableCell>
                 <TableCell> {usuario.cpf} </TableCell>
                 <TableCell>
-                  <Button href={`/usuarios/${usuario._id}`} variant='contained'> 
-                      Editar 
+                  <Button href={`/usuarios/${usuario._id}`} variant='contained'>
+                    Editar
                   </Button>
                 </TableCell>
                 <TableCell>
-                  <Button 
-                  variant='contained'
-                  color='error'
-                  onClick={()=> excluirUsuario(usuario) }
+                  <Button
+                    variant='contained'
+                    color='error'
+                    onClick={() => excluirUsuario(usuario)}
                   >
-                      Deletar
-                  </Button>      
+                    Deletar
+                  </Button>
                 </TableCell>
               </TableRow>)}
           </TableBody>
         </Table>
       </TableContainer>
+      {
+        apagar ? <Mensagem mensagem={mensagem} tipoMensagem="error" /> : ""
+      }
     </>
   )
 }
